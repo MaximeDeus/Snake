42647b0c9a92ddf3936a910783091131b2fd2bf1
Maxime Deus <maxime.deroissart@mail.com>
2020-11-21 21:26:24 +0100

    feat: create Food model, herited from Point model, only image differ

---
+++ src/main/java/com/app/model/Food.java
@@ -0,0 +1,11 @@
+package com.app.model;
+
+import com.app.App;
+import javafx.scene.image.Image;
+
+public class Food extends Point{
+    public Food(double x, double y) {
+        super(x, y);
+        image = new Image(String.valueOf(App.class.getResource("food_apple.png")),20,20,false,false);
+    }
+}
