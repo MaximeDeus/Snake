8ba977146564face9431d27032bc68bb13d34c56
Maxime Deus <maxime.deroissart@mail.com>
2020-11-21 21:29:50 +0100

    fix: update Tools, add method draw, update drawSnake method consequently

--- src/main/java/com/app/Tools.java
+++ src/main/java/com/app/Tools.java
@@ -6,10 +6,15 @@ import javafx.scene.canvas.GraphicsContext;
 
 public class Tools {
 
+    public static void draw(GraphicsContext gc, Point p )
+    {
+        gc.drawImage(p.getImage(), p.getPositionX(), p.getPositionY());
+    }
+
     public static void drawSnake(GraphicsContext gc, Snake snake )
     {
         for (Point p : snake.getBody()) {
-            gc.drawImage(p.getImage(), p.getPositionX(), p.getPositionY());
+            draw(gc,p);
         }
     }
 }
