5ea9b0caeaa43f94572bd5abddb19cb8e7ee021e
Maxime Deus <maxime.deroissart@mail.com>
2020-11-21 02:21:06 +0100

    fix: correctly add class Point

--- src/main/java/com/app/model/Point.java
+++ src/main/java/com/app/model/Point.java
@@ -1,2 +1,40 @@
-package com.app.model;public class Point {
+package com.app.model;
+
+import com.app.App;
+import javafx.scene.image.Image;
+
+/**
+ * Class Point
+ * A point represents a fragment of the snake
+ */
+public class Point {
+    private Image image;
+    private double positionX;
+    private double positionY;
+
+    public Point(double x, double y){
+        image = new Image(String.valueOf(App.class.getResource("snake_body_point.png")),20,20,false,false);
+        positionX = x;
+        positionY = y;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public double getPositionX() {
+        return positionX;
+    }
+
+    public double getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionX(double positionX) {
+        this.positionX = positionX;
+    }
+
+    public void setPositionY(double positionY) {
+        this.positionY = positionY;
+    }
 }
