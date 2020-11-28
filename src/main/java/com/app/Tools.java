c2dba6a5fa103e57727f89f9ac674d7b8b098933
Maxime Deus <maxime.deroissart@mail.com>
2020-11-28 23:50:16 +0100

    doc: update Tools.java, add TODO

--- src/main/java/com/app/Tools.java
+++ src/main/java/com/app/Tools.java
@@ -4,18 +4,23 @@ import com.app.model.Point;
 import com.app.model.Snake;
 import javafx.scene.canvas.GraphicsContext;
 
+// TODO rename to DrawTools
 public class Tools {
 
+    // TODO rename to DrawPoint
     public static void draw(GraphicsContext gc, Point p )
     {
         gc.drawImage(p.getImage(), p.getPositionX(), p.getPositionY());
     }
 
+    // TODO rename to clearPoint
     public static void clear(GraphicsContext gc, Point p )
     {
         // TODO replace values (cf config)
         gc.clearRect(p.getPositionX(), p.getPositionY(), 15,15);
     }
+
+    // TODO add method drawGrid/Clear grid
 
     public static void drawSnake(GraphicsContext gc, Snake snake )
     {
