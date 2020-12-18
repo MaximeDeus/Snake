2f59b881aaf5350cf3463adc0f82882db9675965
Maxime Deus <maxime.deroissart@mail.com>
2020-12-18 01:57:31 +0100

    test: add first test file (actually empty methods)

---
+++ src/main/test/com/app/utils/DrawToolsTest.java
@@ -0,0 +1,43 @@
+package com.app.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DrawToolsTest {
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @Test
+    public void drawPoint() {
+        assertTrue(2 > 1);
+    }
+
+    @Test
+    public void drawHead() {
+    }
+
+    @Test
+    public void drawFood() {
+    }
+
+    @Test
+    public void clearPoint() {
+    }
+
+    @Test
+    public void drawSnake() {
+    }
+
+    @Test
+    public void drawGrid() {
+    }
+
+    @Test
+    public void clear() {
+    }
+}
